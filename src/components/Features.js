3399477c09437c6f44f41cb3d75cbe9871dddc52
Tracy Burge <tracyrburge@gmail.com>
2019-12-19 15:05:53 -0800

    A bit of padding and more informative handling of empty features list

--- src/components/Features.js
+++ src/components/Features.js
@@ -24,52 +24,52 @@ function Features() {
   };
 
   /**
-   * Recursively loops through nested arrays of objects to create lists of 
-   * features and subfeatures.
-   * 
-   * @param {[Object]} featuresList 
-   */
-  const recursivelyListFeatures = (featuresList) => {
-    return featuresList.map((feature) => {
-      const hasSubfeatures = feature.subfeatures.length > 0;
-      const subfeatures = hasSubfeatures ? recursivelyListFeatures(feature.subfeatures) : [];
-
-      const featurePresent = { icon: 'check_circle', color: colors.primary };
-      const featureNotPresent = { icon: 'highlight_off', color: colors.tertiary };
-      const featureIndicator = feature.presence ? featurePresent : featureNotPresent;
-
-      return (
-        <li key={feature.title}>
-          <Icon className="listIcons" style={{color: featureIndicator.color}}>{featureIndicator.icon}</Icon>
-          
-          {hasSubfeatures ?
-            <>
-              {/* Note: this button logic is so specific that it doesn't really make 
-              sense to make it into a component, though if we were going to have similar 
-              drop down components in more than one place I'd consider making a resuable 
-              dropdown component - or finding a library. */}
-              <button className="expandableBtn" onClick={toggleExpandCollapse}>
-                {feature.title}
-                <Icon className="expandList" style={{ color: colors.quaternary }}>keyboard_arrow_right</Icon>
-                <Icon className="collapseList" style={{ color: colors.quaternary }}>keyboard_arrow_down</Icon>
-              </button>
-              
-              <ul className="subfeaturesList">{subfeatures}</ul>
-            </> :
-            feature.title
-          }
-        </li>
-      );
-    });
-  };
-
-  /**
    * After the initial DOM update, create and display the list of features.  
    * We don't REALLY need to use useEffect here since the window.FEATURES 
    * data already exists and we don't have to make a call for it, but this 
    * is what I'd expect to do in a normal situation.  Only called once.
    */
   useEffect(() => {
+    /**
+     * Recursively loops through nested arrays of objects to create lists of 
+     * features and subfeatures.
+     * 
+     * @param {[Object]} featuresList 
+     */
+    const recursivelyListFeatures = (featuresList) => {
+      return featuresList.map((feature) => {
+        const hasSubfeatures = feature.subfeatures.length > 0;
+        const subfeatures = hasSubfeatures ? recursivelyListFeatures(feature.subfeatures) : [];
+
+        const featurePresent = { icon: 'check_circle', color: colors.primary };
+        const featureNotPresent = { icon: 'highlight_off', color: colors.tertiary };
+        const featureIndicator = feature.presence ? featurePresent : featureNotPresent;
+
+        return (
+          <li key={feature.title}>
+            <Icon className="listIcons" style={{ color: featureIndicator.color }}>{featureIndicator.icon}</Icon>
+
+            {hasSubfeatures ?
+              <>
+                {/* Note: this button logic is so specific that it doesn't really make 
+              sense to make it into a component, though if we were going to have similar 
+              drop down components in more than one place I'd consider making a resuable 
+              dropdown component - or finding a library. */}
+                <button className="expandableBtn" onClick={toggleExpandCollapse}>
+                  {feature.title}
+                  <Icon className="expandList" style={{ color: colors.quaternary }}>keyboard_arrow_right</Icon>
+                  <Icon className="collapseList" style={{ color: colors.quaternary }}>keyboard_arrow_down</Icon>
+                </button>
+
+                <ul className="subfeaturesList">{subfeatures}</ul>
+              </> :
+              feature.title
+            }
+          </li>
+        );
+      });
+    };
+
     const features = recursivelyListFeatures(window.FEATURES);
     setFeaturesList(features);
   }, []);
@@ -77,6 +77,7 @@ function Features() {
   return (
     <div className="featuresContainer">
       <ul className="topFeatures">{featuresList}</ul>
+      {featuresList.length === 0 ? 'This listing has no features' : ''}
     </div>
   );
 }
